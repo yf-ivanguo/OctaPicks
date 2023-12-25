8298f34463688e6a92f3518d5d7374ebdbdba025
Smarth Gupta <102758221+straw-hat-gupta@users.noreply.github.com>
2023-12-24 17:52:45 -0800

    fixed col order in cal_names

--- takedown_feats.py
+++ takedown_feats.py
@@ -166,10 +166,10 @@ class TakeDown():
         time_periods = ['l3', 'l5', 'alltime']
 
         # Iterate through all combinations to create column names
-        for fighter in fighters:
-            for significant_strike_stat in significant_strike_stats:
-                for round in rounds:
-                    for time_period in time_periods:
+        for significant_strike_stat in significant_strike_stats:
+            for round in rounds:
+                for time_period in time_periods:
+                    for fighter in fighters:
                         col_name = f"{fighter.replace(' ', '_')}_{significant_strike_stat.replace(' ', '_')}_{round}_{time_period.replace(' ', '_')}"
                         col_names.append(col_name)
         return col_names
@@ -189,10 +189,10 @@ class TakeDown():
         time_periods = ['l3', 'l5', 'alltime']
 
         # Iterate through all combinations to create column names
-        for fighter in fighters:
-            for significant_strike_stat in significant_strike_stats:
-                for round in rounds:
-                    for time_period in time_periods:
+        for significant_strike_stat in significant_strike_stats:
+            for round in rounds:
+                for time_period in time_periods:
+                    for fighter in fighters:
                         col_name = f"{fighter.replace(' ', '_')}_{significant_strike_stat.replace(' ', '_')}_{round}_{time_period.replace(' ', '_')}"
                         col_names.append(col_name)
         return col_names
