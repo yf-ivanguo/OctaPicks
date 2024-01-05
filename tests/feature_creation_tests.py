41c359779bb5912d0c85a00fdf1728822b9853d8
Ivan Guo <ivanguo248@gmail.com>
2024-01-04 17:51:54 -0800

    Added the primary testing file, this will be further populated once #52 has been completed

--- tests/feature_creation_tests.py
+++ tests/feature_creation_tests.py
@@ -2,4 +2,18 @@ import pandas as pd
 
 class FeatureCreationTests():
     def __init__(self) -> None:
-        pass
+        self.fights_df = pd.read_csv('tests/test_fights.csv', encoding='latin-1')
+        self.fighter_df = pd.read_csv('tests/test_fighters.csv', encoding='latin-1')
+        
+    def test_example(self):
+        """
+        This tests the functionality of the X function.
+        """
+
+        # Create the expected values and populate them, can use df.rolling(), etc.
+        expected_vals = []
+
+        # Call feature creation function on the dataframes
+
+        # Assert that the values are equal
+        assert True
